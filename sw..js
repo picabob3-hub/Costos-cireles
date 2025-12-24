f074e712c345a913491217f359e08609c50d05e3
picabob3-hub <picabob3@gmail.com>
2025-12-24 03:22:29 -0500

    Delete sw..js

--- sw..js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('fetch', function(event) {
-  // Este código permite que la app cargue desde la memoria del cel
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
