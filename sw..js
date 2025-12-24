eee7b3f8054d0b94ce0d9580be3a0d88ce9d0b1d
picabob3-hub <picabob3@gmail.com>
2025-12-24 03:23:24 -0500

    Add files via upload

---
+++ sw..js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'cirel-pro-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icono.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
